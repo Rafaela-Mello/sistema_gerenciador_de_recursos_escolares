1f44f148118bab906a70cc5c6192c623c42ba6f4
Ryan Meneses <132396848+ryan-menesesdev@users.noreply.github.com>
2024-11-30 14:53:16 -0300

    SGRE 1.0

---
+++ programação - 1/Main/Main/FormInventario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main
+{
+    public partial class FormInventario : Form
+    {
+        public FormInventario()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rdbCasdratro.Checked == true)
+            {
+                FormCadastro formCadastro = new FormCadastro();
+                formCadastro.ShowDialog();
+            }
+            else if (rdbConsulta.Checked == true)
+            {
+                FormConsulta formConsulta = new FormConsulta();
+                formConsulta.ShowDialog();
+            }
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
